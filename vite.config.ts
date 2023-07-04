9bd6974bf461ed7c76e31b79760c922fb24e8587
asif1122se <35570584+asif1122se@users.noreply.github.com>
2023-07-04 23:10:46 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable spaced-comment */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
+});
